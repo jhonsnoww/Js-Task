74a0a3583b9f069d66bbcd3d72f5d209b7a369b6
jhonsnoww <oochanbg@gmail.com>
2020-02-07 16:04:43 +0630

    gg

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ var numbers =[89,90,20,199];
     if(e > 40){
         return e;
     }
-       
+           
 })
-
+console.log("Map")
 console.log(nus);
 
 var numsFilter = numbers.filter((e)=>{
-    return e;
+    if(e > 50){
+        return true;
+    }
+    return false;
 })
-
+console.log("Filter")
 console.log(numsFilter);
